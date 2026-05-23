1144e37ec585ec0ad67feafb6faefd850d12d246
The-Mars-Guy <The-Mars-Guy@users.noreply.github.com>
2026-05-23 03:28:55 +0000

    deploy: 03a21a6e55691f96d81c26ac0bf2a42b5c701425

---
+++ assets/exerciseLibrary-BHXh3G_X.js
@@ -0,0 +1,1 @@
+import{aq as n,a8 as i}from"./index-BQc1OedL.js";import{E as r}from"./exercise-db-DusN7tRR.js";const f=[...n,...r];function o(e){return n.find(E=>E.id===e)||n.find(E=>i(E.name)===e)||r.find(E=>E.id===e)||r.find(E=>i(E.name)===e)}export{f as FULL_EXERCISE_LIBRARY,o as getFullExerciseById};
